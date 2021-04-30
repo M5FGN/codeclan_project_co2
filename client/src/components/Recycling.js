a0482473580aed74a2077ebceca95df7faeff3b8
Mary Fagan <m5fgn@outlook.com>
2021-04-30 16:17:20 +0100

    Adding Flights form

--- client/src/components/Recycling.js
+++ client/src/components/Recycling.js
@@ -5,7 +5,31 @@ const Recycling = () => {
     return (
 
         <div>
-            This is the Recylcing form.
+            <h2>Recycling</h2>
+            <h4>Choose the value which best describes your recycling activities ...</h4>
+            
+            <form id="recycling_form">
+       
+                <p>
+                <input type="radio" name="recycling_level" id="recycling_all" value="" />
+                <label for="recycling_all">I recycle all of the time.</label>
+                </p>
+                <p>
+                <input type="radio" name="recycling_level" id="recycling_most" value="" />
+                <label for="recycling_most">I recycle most of the time.</label>
+                </p>
+                <p>
+                <input type="radio" name="recycling_level" id="recycling_some" value="" />
+                <label for="recycling_some">I recycle some of the time.</label>
+                </p>
+                <p>
+                <input type="radio" name="recycling_level" id="recycling_none" value="" />
+                <label for="recycling_none">I don't recycle anything.</label>
+                </p>
+
+                <input type="submit" value="Calculate" name="submit" />
+                
+            </form>
         </div>
         
     )
