f617bd11819cc1592188574f4f74eae14982ffc2
Mary Fagan <m5fgn@outlook.com>
2021-04-30 16:33:33 +0100

    Adding Travel form

--- client/src/components/Travel.js
+++ client/src/components/Travel.js
@@ -5,7 +5,55 @@ const Travel = () => {
     return (
 
         <div>
-            This is the Travel form.
+        <h2>Travel to Work or School</h2>
+        <form id="travel_form">
+            <h4>Choose your mode of travel ...</h4>
+            
+            <p>
+            <input type="radio" name="mode_travel" id="train" value="" />
+            <label for="train">Train</label>
+            </p>
+            <p>
+            <input type="radio" name="mode_travel" id="bus" value="" />
+            <label for="bus">Bus</label>
+            </p>
+            <p>
+            <input type="radio" name="mode_travel" id="car" value="" />
+            <label for="car">Car</label>
+            </p>
+            <p>
+            <input type="radio" name="mode_travel" id="bike" value="" />
+            <label for="bike">Bike</label>
+            </p>
+            <p>
+            <input type="radio" name="mode_travel" id="walk" value="" />
+            <label for="walk">Walk</label>
+            </p>
+ 
+            <h4>Input distance travelled ...</h4>
+            <p><input type="number" name="miles_train" id="miles_train" max="50" min="0" />
+            <label for="miles_train">Miles Travelled by Train per day</label>
+            </p>
+
+            <p><input type="number" name="miles_bus" id="miles_bus" max="50" min="0" />
+            <label for="miles_bus">Miles Travelled by Bus per day</label>
+            </p>
+
+            <p><input type="number" name="miles_car" id="miles_car" max="50" min="0" />
+            <label for="miles_car">Miles Travelled by Car per day</label>
+            </p>
+
+            <p><input type="number" name="miles_bike" id="miles_bike" max="50" min="0" />
+            <label for="miles_bike">Miles Cycled Bike per day</label>
+            </p>
+
+            <p><input type="number" name="miles_walk" id="miles_walk" max="50" min="0" />
+            <label for="miles_walk">Miles Walked per day</label>
+            </p>
+       
+            <input type="submit" value="Calculate" name="submit" />
+            
+        </form>
         </div>
 
     )
