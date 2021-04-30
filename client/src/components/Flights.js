8d6c1426d1f61af93a578095e792064cbca75412
Mary Fagan <m5fgn@outlook.com>
2021-04-30 16:08:36 +0100

    Adding Flights form

--- client/src/components/Flights.js
+++ client/src/components/Flights.js
@@ -4,7 +4,20 @@ const Flights = () => {
     
     return (
         <div>
-            This is the flights form.
+            
+            <h2>Flights</h2>
+            <h4>Input the number of hours of airplane travel you have taken in the past 12 months ...</h4>
+            
+            <form id="diet_form">
+
+                <p><input type="number" name="flight_hours" id="flight_hours" max="40" min="0" />
+
+                    <label for="flight_hours">Flight Hours</label>
+                </p>
+        
+                <input type="submit" value="Calculate" name="submit" />
+    
+            </form>
         </div>
     )
 
