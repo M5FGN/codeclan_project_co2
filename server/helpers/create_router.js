137cad51cb4e3857f319f899aef390e6bb5ac6eb
Colin Morrison <colinjmmorris@gmail.com>
2021-05-01 10:09:12 +0100

    cors added to server.js. Routers implemented

---
+++ server/helpers/create_router.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter  = function(collection){
+    
+    const router = express.Router();
+    
+        //get many
+    router.get('/', (req, res) => {
+      collection.find()
+      .toArray()
+      .then((docs)=> res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+    //get one
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectID(id) })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+//post one
+  router.post('/', (req, res) =>{
+    const users = req.body;
+    collection
+    .insertOne(users)
+    .then((result)=> res.json(result.ops[0]))
+    .catch((err)=>{
+      console.error(err);
+      res.status(500);
+      res.json({status:500, error: err})
+    });
+    
+  });
+
+    //delete one
+  router.delete('/:id', (req, res) =>{
+    const id = req.params.id;
+    collection
+      .deleteOne({_id: ObjectID(id)})
+      .then(result => res.json(result))
+      .catch((err)=>{
+        console.error(err);
+        res.status(500);
+        res.json({status:500, error: err})
+      });
+  })
+
+  return router;
+};
+
+module.exports = createRouter;
