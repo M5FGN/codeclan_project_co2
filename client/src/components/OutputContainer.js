a3e6bb4fef7f5de378f2c580f6844d7e4bcc76ef
Mary Fagan <m5fgn@outlook.com>
2021-05-02 15:02:29 +0100

    Update Output Container

--- client/src/components/OutputContainer.js
+++ client/src/components/OutputContainer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Chart from '../components/Chart';
+import Output from '../components/Output';
+
 
 const OutputContainer = () => {
 
     return (
         <div>
         This is the Output Side.
+        < Chart />
+        This is where the Output will go.
+        {/* < Output /> */}
         </div>
     )
 
