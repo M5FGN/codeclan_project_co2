f20b25c620f580e23435bcd9811c5a419492949f
Colin Morrison <colinjmmorris@gmail.com>
2021-05-02 23:08:24 +0100

    Output render samples modified to suit changes

--- client/src/components/Output.js
+++ client/src/components/Output.js
@@ -8,7 +8,7 @@ const Output = ({user, removeUser, totalCarbon}) => {
         deleteUser(user._id);
         removeUser(user._id);
     }
-    // Values below are for rendering
+
     const diet = user.footprint.diet;
     const air = user.footprint.air;
     const heating = user.footprint.heating;
@@ -19,6 +19,29 @@ const Output = ({user, removeUser, totalCarbon}) => {
     const cycle = user.footprint.commute.cycling;
     const walk = user.footprint.commute.walk;
 
+// The below is a trial for looping through the data to render it, may be useful, doesn't work yet
+
+    // const data = {
+    //     diet: user.footprint.diet,
+    //     air: user.footprint.air,
+    //     heating:  user.footprint.heating,
+    //     recycling: user.footprint.recycling,
+    //     car: user.footprint.commute.car,
+    //     train: user.footprint.commute.train,
+    //     bus: user.footprint.commute.bus,
+    //     cycle: user.footprint.commute.cycling,
+    //     walk: user.footprint.commute.walk 
+    // }
+
+    // const dataLoop = () => {
+    //     for (let [key, value] in data){
+    //         if (!isNaN(value) && value !== null){
+    //             return <li>Footprint - {key} {value}</li>
+    //         }
+    //     }
+    // }
+
+
     return (
         <div>
             <ul>
@@ -26,6 +49,13 @@ const Output = ({user, removeUser, totalCarbon}) => {
                 <li>Full name: {user.forename} {user.surname}</li>
 
                 {/* This should be a loop, but is only here for example data  */}
+                {/* FOR INFO:
+                    .The data input by default is a string. We  have changed all input data to be parsed into a number before storage.
+                    .This means that any blank entries not filled in, are stored as 'NaN'
+                    .And initially empty ones are 'null' when we make a new user
+                    .So we only want to render those where the data is a number i.e. isNaN is false, so !isNaN is true.
+                    .And where data does not equal null
+                */}
                 {!isNaN(diet) && diet !== null ? <li>Footprint - Diet: {diet}</li> : null}
                 {!isNaN(air) && air !== null ? <li>Footprint - Air travel: {air}</li> : null}
                 {!isNaN(heating) && heating !== null ? <li>Footprint - Heating: {heating}</li> : null }
