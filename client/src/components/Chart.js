dd1baaf6baae815e24f8e26c162027928d1f132a
Colin Morrison <colinjmmorris@gmail.com>
2021-05-04 13:05:31 +0100

    Chart user numbers working, no average yet

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -17,28 +17,48 @@ import { render } from 'react-dom'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
-
 // Code to be changed
 
 // For a new User the Initial State of Data and Title should show the Average CO2 Footprint - prepopulate list as below (with correct numbers)
 // Once a new user adds Diet the list should delete all other elements and only show Diet, for each form the user completes a new line 
         //should be added to the list with the relevant key and value.
-// For an Existing User the List should populate with the figures they have entered previously then update as they change the numbers. 
-        //The title should be "Your Carbon Footprint"
 
-const data = [
-  ['Diet', 20],
-  ['Travel', 20],
-  ['Flights', 20],
-  ['Heating', 20],
-  ['Recycling', 20]]
+// For an Existing User the List should populate with the figures they have entered previously then update as they change the numbers. - DONE
+        //The title should be "Your Carbon Footprint" - DONE (ish. Only renders if we have a user)
+ 
+const Chart = ({user}) => 
+{
+  const diet = user.footprint.diet;
+  const air = user.footprint.air;
+  const heating = user.footprint.heating;
+  const recycling = user.footprint.recycling;
+  const car = user.footprint.commute.car; // Remove this line and uncomment below one when github is up to date with new travelTotal
+  // const travelTotal = user.footprint.commute.travelTotal;
 
-  const title = 'Average<br>Carbon<br>Foot Print'
-// 'Your<br>Carbon<br>Foot Print'
+  
+  let title = '';
+  const chooseTitle = () => {
+    // If diet is null, this means there is no user data yet. So render the average
+    // If diet is not null, this means we have some user data, so render their personal chart
+    if (diet !== null){
+      title = 'Your <br> Carbon <br> Footprint'
+    };
+    if (diet === null){
+      title = 'Average <br> Carbon <br> Footprint'
+    };
+  }
+  chooseTitle()
 
-// End of Code needing changed
 
-const options = {
+  const data = [
+    ['Diet', diet],
+    ['Travel', car], // This will need changed to travelTotal once my pull has been merged
+    ['Flights', air],
+    ['Heating', heating],
+    ['Recycling', recycling]
+  ];
+
+  const options = {
     chart: {
       plotBackgroundColor: null,
       plotBorderWidth: 0,
@@ -81,13 +101,16 @@ const options = {
       data: data
     }]
   }
+
+  return (
+    <div>
+      <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      />
+    </div>
+    )
+  }
  
-const Chart = () => <div>
-  <HighchartsReact
-    highcharts={Highcharts}
-    options={options}
-  />
-</div>
- 
-render(<Chart />, document.getElementById('root'))
+// render(<Chart />, document.getElementById('root'))
 export default Chart;
